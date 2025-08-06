87a50a5564e3bb46c7ed269546bbe0770321f5ed
Jaswanth Revuri <jaswanth.revuri@sky.uk>
2025-08-06 10:09:34 +0100

    added editorial code

---
+++ EDITORIAL_UK/EDITORIAL_DASHBOARD_REBUILD_OVERVIEW (1).sql
@@ -0,0 +1,17 @@
+
+CREATE OR REPLACE TABLE `sdp-sandbox-nowtv-int.SGL18.EDITORIAL_DASHBOARD_REBUILD_OVERVIEW` AS
+
+select 
+  adobe_date,
+  geo_country,
+  device_type,
+  entry_app_section,
+  sum(case when visit is null then 0 else visit end) as total_visits,
+  sum(case when click is null then 0 else tile_click end) as total_clicks,
+  sum(case when player_click is null then 0 else player_click end) as total_plays,
+  count(distinct visitor_id) as total_users
+from `sdp-sandbox-nowtv-int.SGL18.EDITORIAL_DASHBOARD_REVAMP_RAW_TABLE`
+where entry_app_section is not null
+and entry_app_section not in ('live-tv')
+group by 1,2,3,4
+order by 1,2,3,4
